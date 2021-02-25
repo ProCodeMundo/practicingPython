5f694d8f81ddd7c074a02511407076a9f421279d
ProCodeMundo <58192099+ProCodeMundo@users.noreply.github.com>
2021-02-24 19:03:32 -0500

    Add files via upload

---
+++ ifStatement.py
@@ -0,0 +1,15 @@
+"""
+Assign 8 to the variable x and 15 to the variable y.
+In the same cell, create 2 conditional statements.
+Let the first one print "At least one of the conditions is satisfied." if x is greater than 3 or y is even.
+Let the second one print "Neither condition is satisfied." if x is less than or equal to 3 and y is odd.
+"""
+x = 8
+y = 15
+
+if x > 3 or y %2 ==0:
+    print("At least one of the conditions is satisfied.")
+elif x <=3 and y %2==1:
+    print("Neither condition is satisfied.")
+# 
+
